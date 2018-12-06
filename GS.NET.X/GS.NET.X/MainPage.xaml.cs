685d79812118fecc71580402561994cbdafee28b
Dillon Newell <dillon_newell@live.ca>
2018-12-06 15:24:53 -0500

    add android

---
+++ GS.NET.X/GS.NET.X/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameSolver.NET.Benchmarking;
+using Xamarin.Forms;
+
+namespace GS.NET.X
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ResultsLabel.Text = "";
+            if (true)
+            {
+                foreach (var x in Benchmark.Pure())
+                {
+                    ResultsLabel.Text += "\n" + x;
+                }
+            }
+        }
+    }
+}
--- GameSolver.NET.Benchmarking/Benchmark.cs
+++ GameSolver.NET.Benchmarking/Benchmark.cs
@@ -10,17 +10,17 @@ namespace GameSolver.NET.Benchmarking
     {
         private static Random _r = new Random(65846);
 
-        public static IEnumerable<string> Pure(int powers = 5)
+        public static IEnumerable<string> Pure(int powers = 5, int values = 10)
         {
-            return Bench(PureImpl, powers);
+            return Bench(PureImpl, powers, values);
         }
 
-        public static IEnumerable<string> Mixed(int powers = 5)
+        public static IEnumerable<string> Mixed(int powers = 5, int values = 10)
         {
-            return Bench(MixedImpl, powers);
+            return Bench(MixedImpl, powers, values);
         }
 
-        private static IEnumerable<string> Bench(Func<int, (TimeSpan, TimeSpan, int, long)> func, int powers)
+        private static IEnumerable<string> Bench(Func<int, int, (TimeSpan, TimeSpan, int, long)> func, int powers, int values)
         {
             var s = new Stopwatch();
 
@@ -32,12 +32,11 @@ namespace GameSolver.NET.Benchmarking
                 var tsCpu = new TimeSpan();
                 var count = 0;
 
-                var mem = 0l;
-                var me = Process.GetCurrentProcess();
+                var mem = 0L;
 
                 for (var j = 0; j < 10; j++)
                 {
-                    var x = func(length);
+                    var x = func(length, values);
 
                     tsReal += x.Item1;
                     tsCpu += x.Item2;
@@ -56,15 +55,15 @@ namespace GameSolver.NET.Benchmarking
             }
         }
 
-        private static (TimeSpan, TimeSpan, int, long) PureImpl(int length)
+        private static (TimeSpan, TimeSpan, int, long) PureImpl(int length, int values)
         {
             var s = new Stopwatch();
             var me = Process.GetCurrentProcess();
 
             var m = new double[length][];
             var n = new double[length][];
-            var o = RandomColumns(length).ToArray();
-            var p = RandomColumns(length).ToArray();
+            var o = RandomColumns(length, values).ToArray();
+            var p = RandomColumns(length, values).ToArray();
             for (var k = 0; k < length; k++)
             {
                 m[k] = new double[length];
@@ -95,14 +94,14 @@ namespace GameSolver.NET.Benchmarking
             return (s.Elapsed, tCpu, x.Count, me.WorkingSet64);
         }
 
-        private static (TimeSpan, TimeSpan, int, long) MixedImpl(int length)
+        private static (TimeSpan, TimeSpan, int, long) MixedImpl(int length, int values)
         {
             var s = new Stopwatch();
             var me = Process.GetCurrentProcess();
 
             var m = new double[2][];
-            m[0] = RandomColumns(length).ToArray();
-            m[1] = RandomColumns(length).ToArray();
+            m[0] = RandomColumns(length, values).ToArray();
+            m[1] = RandomColumns(length, values).ToArray();
 
             var t = new TwoPlayerZeroSum(m);
 
@@ -115,10 +114,10 @@ namespace GameSolver.NET.Benchmarking
             return (s.Elapsed, tCpu, 1, me.WorkingSet64);
         }
 
-        private static IEnumerable<double> RandomColumns(int amount)
+        private static IEnumerable<double> RandomColumns(int amount, int values)
         {
             for (var i = 0; i < amount; i++)
-                yield return _r.Next(100);
+                yield return _r.Next(values);
         }
     }
 }
