e700d72acf0597beb7313e76bf410954514ae072
Dillon Newell <dillon_newell@live.ca>
2018-12-07 13:43:46 -0500

    test example

--- GameSolver.NET/Program.cs
+++ GameSolver.NET/Program.cs
@@ -11,6 +11,35 @@ namespace GameSolver.NET
     {
         private static void Main(string[] args)
         {
+            var a = new []
+            {
+                new[] { double.PositiveInfinity, 2, 3, 5 },
+                new[] { 2, double.PositiveInfinity, -3, -1 },
+                new[] { 3, -3, double.PositiveInfinity, 0 },
+                new[] { 5, -1, 0, double.PositiveInfinity }
+            };
+
+            var b = new double[4][];
+            for (var i = 0; i < 4; i++)
+            {
+                b[i] = new double[4];
+                for (var j = 0; j < 4; j++)
+                {
+                    if (i == j)
+                    {
+                        b[i][j] = double.PositiveInfinity;
+                    }
+                    else
+                    {
+                        b[i][j] = -a[i][j];
+                    }
+                }
+            }
+
+            var t = new TwoPlayerSolver(a, b);
+
+            var s = t.BruteForceSolutions().ToList();
+
             if (true)
             {
                 Console.WriteLine("Pure:");
